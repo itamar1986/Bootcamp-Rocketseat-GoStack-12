deaf5c17e5daf6ca9301b3f4d07d26365ae261b4
Itamar1986 <itamar_minnow@hotmail.com>
2021-02-19 20:35:46 -0300

    Finalizando Front-end com ReactJS

--- 02-conceitos-dev/02-frontend/src/App.js
+++ 02-conceitos-dev/02-frontend/src/App.js
@@ -5,45 +5,38 @@ import './App.css';
 
 import Header from './components/Header';
 
-/**
- * Componente
- * Propriedade
- * Estado & Imutabilidade
- */
-
 function App() {
-    const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-        api.get('projects').then(response => {
-            setProjects(response.data);
-        });
-    }, []);
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    })
+  }, []);
 
-    async function handleAppProject() {
-        //setProjects([...projects,`Novo projeto ${Date.now()}`]);
+  async function handleAddProject() {
+    //setProjects([...projects, `Novo Projeto ${Date.now()}`]);
 
-        const response = await api.post('projects', {
-            title: `Novo projeto ${Date.now()}`,
-            owner: "Itamar"
-        });
+    const response = await api.post('projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Itamar Silva'
+    });
 
-        const project = response.data;
+    const project = response.data;
 
-        setProjects([...projects, project]);
-    }
+    setProjects([...projects, project]);
+  }
 
-    return (
-        <>
-            <Header title="Projects"/>
+  return (
+    <>
+      <Header title="Projects" />
 
-            <ul>
-                {projects.map(project => <li key={project.id}>{project.title}</li>)}
-            </ul>
-
-            <button type="button" onClick={handleAppProject}>Adicionar projeto</button>
-        </>
-    );
+      <ul>
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
+      </ul>
+      <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+    </>
+  );
 }
 
 export default App;
--- 02-conceitos-dev/02-frontend/src/index.js
+++ 02-conceitos-dev/02-frontend/src/index.js
@@ -3,6 +3,4 @@ import { render } from 'react-dom';
 
 import App from './App';
 
-// JSX: HTML dentro do JavaScript (Javascript XML)
-
 render(<App />, document.getElementById('app'));
